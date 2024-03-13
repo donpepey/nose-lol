4644f7e298bee0296e0ebd14c19a80c2497eb503
donp <163166993+donpepey@users.noreply.github.com>
2024-03-13 15:55:34 -0300

    Add files via upload

---
+++ Clase3lol.py
@@ -0,0 +1,13 @@
+import datetime
+pepe = input("Ingresa tu nombre -- > ")
+pepoide = input ("Ingresa tu apellido -- >")
+alo= input ("Ingresa tu edad -- > ")
+salamaleco = input("Pais donde vives --> ")
+sala = input("Ciudad donde vives -- > ")
+print("Resultados:")
+print("Nombre: "+pepe)
+print("Apellido: "+pepoide)
+print("Edad: "+alo)
+print("Pais: "+salamaleco)
+print("Ciudad: "+sala)
+print("Registrado el:",datetime.datetime.now())
